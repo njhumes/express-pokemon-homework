50948c91d24109f42724eae3cc3b7bab90e45aaf
Nicholas Humes <njhumes@gmail.com>
2019-01-15 18:34:34 -0700

    Commit 1, My server is up and running

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PokemonList = require('./models/pokemon');
+
+
+app.get('/pokemon', (req, res) => {
+    res.render('index.ejs', {
+        pokemonKey: PokemonList
+    })
+});
+
+app.get('/pokemon/:id', (req, res) => {
+    res.render('show.ejs', {
+        pokemonKey: PokemonList[req.params.id]
+    })
+});
+
+
+
+app.listen(3000, () => {
+    console.log('Express Pokemon HW Server is listening on port 3000');
+});
+
